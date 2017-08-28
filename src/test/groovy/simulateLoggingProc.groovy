ac2d5b7950e099c7ef7be732afde1e67124d6d3d
renato <renato@twobo.com>
2017-08-28 20:24:19 +0200

    Created script that simulates a logging source.

---
+++ src/test/groovy/simulateLoggingProc.groovy
@@ -0,0 +1,37 @@
+/**
+ * This script simulates a process that generates predictable logging output.
+ */
+if ( args.size() != 1 ) {
+    println 'Wrong number of arguments (expected 1, got ' + args.size() +
+            ').\nUsage: simulateLoggingProc <fileName>'
+    System.exit( 1 )
+}
+
+file = new File( args[ 0 ] )
+
+if ( file.exists() ) {
+    file.delete()
+}
+
+def write = { String msg ->
+    file << 'INFO ' + new Date() + ' - ' + msg + '\n'
+}
+
+write 'Sleeping 5 seconds...'
+sleep 5_000
+
+write 'Logging 10x padLeft(n, n.toString)'
+
+10.times { it -> write( it.toString().padLeft( it, it.toString() ) ) }
+
+write 'Sleeping 5 seconds'
+sleep 5_000
+
+write 'Logging numbers 50 to 1, one per second'
+
+( 50..1 ).each {
+    write it.toString()
+    sleep 1_000
+}
+
+write 'Good bye!'
