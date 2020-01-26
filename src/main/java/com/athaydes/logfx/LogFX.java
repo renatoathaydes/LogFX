c3264cbdcf3391ab80544d55e0768760d472d683
renatoathaydes <renatoathaydes@hotmail.com>
2020-01-26 15:48:23 +0100

    Updated Font-Awesome to 5.0.

--- src/main/java/com/athaydes/logfx/LogFX.java
+++ src/main/java/com/athaydes/logfx/LogFX.java
@@ -333,7 +333,7 @@ public class LogFX extends Application {
             SetupMacTrayIcon.run();
         }
 
-        Font.loadFont( LogFX.class.getResource( "/fonts/fontawesome-webfont.ttf" ).toExternalForm(), 12 );
+        Font.loadFont( LogFX.class.getResource( "/fonts/Font-Awesome-5-Free-Regular-400.otf" ).toExternalForm(), 12 );
         Application.launch( LogFX.class );
     }
 
