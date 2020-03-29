a26412fd5a25c53ed1e33681f22294903748b485
renatoathaydes <renatoathaydes@hotmail.com>
2020-03-29 17:25:33 +0200

    Adding Dart script to manage website branch.

---
+++ checkoutWebsite.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+main(List<String> args) async {
+  final siteBranch = 'website';
+  if (!(await branchExists(siteBranch))) {
+    if (args.length == 1 && args[0] == '-f') {
+      await createEmptyBranch(siteBranch);
+    } else {
+      print('$siteBranch branch does not exist. Run with -f to create it.');
+      exit(1);
+    }
+  }
+  await cleanup(branch: siteBranch, dir: siteBranch);
+  await checkout(branch: siteBranch, dir: siteBranch);
+  print('Done!');
+}
+
+Future<void> cleanup({String branch, String dir}) async {
+  print('Cleaning up directory "$dir" and worktree!');
+  final branchDir = Directory(dir);
+  if ((await branchDir.exists())) {
+    await branchDir.delete(recursive: true);
+  }
+  await branchDir.create();
+  ['git', 'worktree', 'prune'].execute();
+  final workTreeDir = Directory('.git/worktrees/$dir');
+  if ((await workTreeDir.exists())) {
+    await workTreeDir.delete(recursive: true);
+  }
+}
+
+Future<void> checkout({String branch, String dir}) async {
+  print('Checking out branch "$branch" into directory "$dir"!');
+  ['git', 'worktree', 'add', '-B', branch, dir, 'origin/$branch'].execute();
+}
+
+Future<String> currentBranch() {
+  return ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+      .execute()
+      .then((p) => p.stdout.toString().trim());
+}
+
+Future<bool> branchExists(String name) async {
+  final res = (await ['git', 'branch', '--list', name].execute());
+  return RegExp('([+\\-*]\\s+)?$name').hasMatch(res.stdout.toString().trim());
+}
+
+Future<void> createEmptyBranch(String name) async {
+  final branch = await currentBranch();
+  print('Creating empty branch with name "$name"');
+  await ['git', 'checkout', '--orphan', name].execute();
+  await ['git', 'reset', '--hard'].execute();
+  await ['git', 'commit', '--allow-empty', '-m', 'Initializing gh-pages branch']
+      .execute();
+  print('Pushing branch "$name"');
+  await ['git', 'push', 'origin', name].execute();
+  await ['git', 'checkout', branch].execute();
+}
+
+extension on List<String> {
+  Future<ProcessResult> execute({bool checkStatus = true}) async {
+    final res =
+        await Process.run(this.first, this.skip(1).toList(), runInShell: true);
+    if (checkStatus && res.exitCode != 0) {
+      stderr.writeln('Command exited with ${res.exitCode}: $this');
+      print(res.stdout);
+      stderr.writeln(res.stderr);
+      exit(res.exitCode);
+    }
+    return res;
+  }
+}
