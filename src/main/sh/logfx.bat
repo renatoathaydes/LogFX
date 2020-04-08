9e87bd7a6eb8f3af30fc898aab5aa08c8ef62bc3
renatoathaydes <renatoathaydes@hotmail.com>
2020-04-08 19:58:47 +0200

    Use serial-GC as it has slightly lower memory consumption. It is also appropriate for a small heap.

--- src/main/sh/logfx.bat
+++ src/main/sh/logfx.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR=%~dp0
-set VM_OPTIONS=-splash:%DIR%logfx-logo.png -Xms32m
+set VM_OPTIONS=-splash:%DIR%logfx-logo.png -Xms32m -XX:+UseSerialGC
 "%DIR%java" %VM_OPTIONS% -m com.athaydes.logfx/com.athaydes.logfx.LogFX %* && exit 0
