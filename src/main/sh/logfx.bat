7e41608be881978b4ea8ac910164e09ca4ac127f
renatoathaydes <renatoathaydes@hotmail.com>
2020-04-08 19:03:47 +0200

    Fixes to make the splasher run successfully on CI. Added Xms option to shell scripts and fixed Windows bat script.

--- src/main/sh/logfx.bat
+++ src/main/sh/logfx.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR=%~dp0
-set VM_OPTIONS="-splash:%DIR%/logfx-logo.png"
-"%DIR%\java" %VM_OPTIONS% -m com.athaydes.logfx/com.athaydes.logfx.LogFX %* && exit 0
+set VM_OPTIONS=-splash:%DIR%logfx-logo.png -Xms32m
+"%DIR%java" %VM_OPTIONS% -m com.athaydes.logfx/com.athaydes.logfx.LogFX %* && exit 0
