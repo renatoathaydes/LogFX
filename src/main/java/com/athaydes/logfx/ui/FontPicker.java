c6f412208609a8ece64e0b0938d462b29dd33ce3
Renato Athaydes <renato@athaydes.com>
2021-09-11 14:53:34 +0200

    Made font selector more consistent with other dialogs.

--- src/main/java/com/athaydes/logfx/ui/FontPicker.java
+++ src/main/java/com/athaydes/logfx/ui/FontPicker.java
@@ -3,10 +3,14 @@ package com.athaydes.logfx.ui;
 import com.athaydes.logfx.binding.BindableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.stage.StageStyle;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.rangeClosed;
@@ -14,36 +18,46 @@ import static javafx.collections.FXCollections.observableList;
 
 public class FontPicker {
 
-    public static Dialog showFontPicker(BindableValue<Font> fontProperty) {
+    public static Dialog showFontPicker( BindableValue<Font> fontProperty ) {
         ComboBox<String> fontNames = new ComboBox<>(
-                observableList(Font.getFontNames().stream().distinct().collect(toList())));
-        fontNames.getSelectionModel().select(fontProperty.getValue().getName());
+                observableList( Font.getFontNames().stream().distinct().collect( toList() ) ) );
+        fontNames.getSelectionModel().select( fontProperty.getValue().getName() );
 
         ComboBox<Double> fontSizes = new ComboBox<>(
-                observableList(rangeClosed(6, 42)
+                observableList( rangeClosed( 6, 42 )
                         .asDoubleStream().boxed()
-                        .collect(toList()))
+                        .collect( toList() ) )
         );
-        fontSizes.getSelectionModel().select(fontProperty.getValue().getSize());
+        fontSizes.getSelectionModel().select( fontProperty.getValue().getSize() );
 
-        EventHandler<ActionEvent> eventHandler = (event) ->
-                fontProperty.setValue(new Font(fontNames.getValue(), fontSizes.getValue()));
+        EventHandler<ActionEvent> eventHandler = ( event ) ->
+                fontProperty.setValue( new Font( fontNames.getValue(), fontSizes.getValue() ) );
 
-        fontNames.setOnAction(eventHandler);
-        fontSizes.setOnAction(eventHandler);
+        fontNames.setOnAction( eventHandler );
+        fontSizes.setOnAction( eventHandler );
 
-        GridPane grid = new GridPane();
-        grid.setVgap(5);
-        grid.add(new Label("Font name:"), 0, 0);
-        grid.add(fontNames, 1, 0);
-        grid.add(new Label("Font size:"), 0, 1);
-        grid.add(fontSizes, 1, 1);
+        var grid = new GridPane();
+        grid.setVgap( 5 );
 
-        Dialog dialog = new Dialog(grid);
-        dialog.setTitle("Select a font");
-        dialog.setAlwaysOnTop(true);
-        dialog.setResizable(false);
+        var columnConstraints = new ColumnConstraints( 45 );
+        grid.getColumnConstraints().add( columnConstraints );
+
+        grid.add( new Label( "Font:" ), 0, 0 );
+        grid.add( fontNames, 1, 0 );
+        grid.add( new Label( "Size:" ), 0, 1 );
+        grid.add( fontSizes, 1, 1 );
+
+        Dialog dialog = new Dialog( new Label( "Select a font" ), grid );
+        dialog.setStyle( StageStyle.UNDECORATED );
+        dialog.setAlwaysOnTop( true );
+        dialog.setResizable( false );
         dialog.makeTransparentWhenLoseFocus();
+
+        var paneButtons = new VBox( 10 );
+        var closeButton = new Button( "Close" );
+        closeButton.setOnAction( event -> dialog.hide() );
+        paneButtons.getChildren().add( closeButton );
+        dialog.getBox().getChildren().add( paneButtons );
 
         dialog.show();
         return dialog;
