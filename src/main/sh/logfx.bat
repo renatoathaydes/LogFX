07e9136f70eb436620b56110fe757d6c5770750b
renatoathaydes <renato@athaydes.com>
2025-08-26 19:19:59 +0200

    Allow calling logfx with VM_OPTIONS already set.

--- src/main/sh/logfx.bat
+++ src/main/sh/logfx.bat
@@ -2,5 +2,5 @@
 
 set DIR=%~dp0
 
-set VM_OPTIONS=-Xms32m -XX:+UseSerialGC
+set VM_OPTIONS=%VM_OPTIONS% -Xms32m -XX:+UseSerialGC
 "%DIR%java" -splash:"%DIR%logfx-logo.png" %VM_OPTIONS% -m com.athaydes.logfx/com.athaydes.logfx.LogFX %*
